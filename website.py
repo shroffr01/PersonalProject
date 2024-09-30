58f174d6265cedaf3f32c91ef90b9b3eb0809c59
Rohan Shroff <rs7aq@virginia.edu>
2024-09-30 16:57:26 -0400

    c

--- website.py
+++ website.py
@@ -188,13 +188,13 @@ def phone():
         st.write("Selected City:", selected_city)
 
         selected_city1 = st.selectbox("Select destination", df['city_ascii, state_id'].unique())
-        st.write("Selected City:", selected_city)
+        st.write("Selected City:", selected_city1)
         checkbox_val = st.checkbox("Form checkbox")
 
         # Every form must have a submit button.
         submitted = st.form_submit_button("Submit")
         if submitted:
-            st.write("slider", slider_val, "checkbox", checkbox_val)
+            st.write("slider", checkbox_val, "checkbox", checkbox_val)
         st.write("Outside the form")
 
 # Defines streamlit page names
