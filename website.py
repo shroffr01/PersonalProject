e54017e29e57b7b3a2584e8880c108c72c28174a
Rohan Shroff <rs7aq@virginia.edu>
2024-09-30 16:53:48 -0400

    hi

--- website.py
+++ website.py
@@ -176,7 +176,17 @@ def page1():
     weather_forecast(selected_lat, selected_lon, options)
 
 def phone():
-    st.write('bye')
+    
+    with st.form("my_form"):
+        st.write("Inside the form")
+        slider_val = st.slider("Form slider")
+        checkbox_val = st.checkbox("Form checkbox")
+
+        # Every form must have a submit button.
+        submitted = st.form_submit_button("Submit")
+        if submitted:
+            st.write("slider", slider_val, "checkbox", checkbox_val)
+        st.write("Outside the form")
 
 # Defines streamlit page names
 page_names_to_funcs = {
