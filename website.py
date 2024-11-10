973822634a8900c6038a356cd53a98f836bd1d8e
Rohan Shroff <rs7aq@virginia.edu>
2024-11-10 14:59:38 -0500

    c

--- website.py
+++ website.py
@@ -483,8 +483,8 @@ def route_planner():
                 alerts_list = []
 
                 for a in range(len(df_alert)):
-                    desired_val = pd.to_datetime(desired_val)
-                    desired_val = desired_val[0].strftime('%Y-%m-%d %H:%M:%S')
+                    #desired_val = pd.to_datetime(desired_val)
+                    desired_val = desired_val.strftime('%Y-%m-%d %H:%M:%S')
                     desired_val = pd.to_datetime(desired_val)
                     st.text(desired_val)
                     st.text(df_alert['start'][a])
