bc8176ef6f7e71b0d599ac750c983880a5a8455e
Rohan Shroff <rs7aq@virginia.edu>
2024-11-09 23:28:39 -0500

    c

--- website.py
+++ website.py
@@ -454,7 +454,8 @@ def route_planner():
             desired_val = [route_info_df['date_time'][i]]
 
             API_key = '6e4a8336ddea630116c32b827c5226be'
-            url = f"url = f'https://api.openweathermap.org/data/3.0/onecall?lat={route_info_df['lat'][i]}&lon={route_info_df['lon'][i]}&exclude=current,minutely,daily&appid={API_key}'"
+            
+            url = f'https://api.openweathermap.org/data/3.0/onecall?lat={route_info_df['lat'][i]}&lon={route_info_df['lon'][i]}&exclude=current,minutely,daily&appid={API_key}'
             r = requests.get(url, headers = headers)  
             myjson = json.loads(r.text)
 
@@ -465,7 +466,7 @@ def route_planner():
 
             weather_info_df = pd.Concat([weather_info_df, df_main])
         st.dataframe(weather_info_df)
-        
+
     if selected_starting_point != None:
         weather_data = collect_weather_data1(route_info_df)
     
